59f9344e6f4c10e5649f60a1e6a87ef4f1a61e3d
Princeelvis29 <princeelvis29@gmail.com>
2026-09-09 23:19:11 +0100

    Update dark mode UI globally and add Firebase background handler

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,22 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
+// 1. ADDED TOP-LEVEL FUNCTION FOR FIREBASE BACKGROUND MESSAGING
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+  print("Handling a background message: ${message.messageId}");
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
   await Firebase.initializeApp();
+  
+  // 2. REGISTERED THE BACKGROUND HANDLER HERE
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   
   FirebaseMessaging messaging = FirebaseMessaging.instance;
   await messaging.requestPermission(
